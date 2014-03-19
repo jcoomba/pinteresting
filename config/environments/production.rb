21e43dfb3a6a9a365f006cbe24e20536a9ad5d9a
jcoomba <jason@meuspics.com>
2014-03-19 20:28:11 -0300

    Added Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Heroku/Devise 
+  # Note to set this link to our actual host name
+  config.action_mailer.default_url_options = { host: 'http://dona-branca.herokuapp.com' }
 end
